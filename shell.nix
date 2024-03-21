68278d252d70a043c1d736df7d50f9c56320599d
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-03-21 10:04:19 +0100

    trying new library

--- shell.nix
+++ shell.nix
@@ -4,7 +4,14 @@
     pkg-config
     libdbusmenu
     dbus
+    glib.dev 
     cargo
     rustc
+    cairo
+    gdk-pixbuf
+    pango
+    gtk3
+    atk
+	gcc
   ];
 }
--- src/main.rs
+++ src/main.rs
@@ -1,134 +1,34 @@
-use arboard::Clipboard;
-use ksni::{self, Icon};
-use std::io::Cursor;
-
-const BUFFER_LENGTH: usize = 10;
-const LINE_LENGTH: usize = 30;
-const ICON_WIDTH: i32 = 32;
-const ICON_HEIGHT: i32 = 32;
-
-#[derive(Debug)]
-struct MyTray {
-	items: Vec<String>,
-}
-
-impl ksni::Tray for MyTray {
-	fn icon_pixmap(&self) -> Vec<Icon> {
-		let cursor_icon = Cursor::new(include_bytes!("../resources/icon.png"));
-		let decoder_icon = png::Decoder::new(cursor_icon);
-		let (info_icon, mut reader_icon) =
-			decoder_icon.read_info().expect("Failed reading icon data");
-		let mut buf_icon = vec![0; info_icon.buffer_size()];
-		reader_icon
-			.next_frame(&mut buf_icon)
-			.expect("Failed getting icon frame data");
-
-		vec![Icon {
-			width: ICON_WIDTH,
-			height: ICON_HEIGHT,
-			data: buf_icon,
-		}]
-	}
-	fn title(&self) -> String {
-		{ "MyTray" }.into()
-	}
-	fn id(&self) -> String {
-		env!("CARGO_PKG_NAME").into()
-	}
-	fn menu(&self) -> Vec<ksni::MenuItem<Self>> {
-		use ksni::menu::*;
-
-		let item_menu: Vec<ksni::MenuItem<Self>> = self
-			.items
-			.iter()
-			.map(|item| {
-				let item_clone = item.clone();
-				StandardItem {
-					label: format_title(&item_clone),
-					activate: Box::new(move |_| set_clipboard_text(&item_clone)),
-					..Default::default()
-				}
-				.into()
-			})
-			.collect();
-
-		let mut menu = Vec::new();
-		menu.push(
-			SubMenu {
-				label: "Susuwatari".into(),
-				..Default::default()
-			}
-			.into(),
-		);
-		menu.push(MenuItem::Separator);
-		menu.extend(item_menu);
-		menu.push(MenuItem::Separator);
-		menu.push(
-			StandardItem {
-				label: "Exit".into(),
-				icon_name: "application-exit".into(),
-				activate: Box::new(|_| std::process::exit(0)),
-				..Default::default()
-			}
-			.into(),
-		);
-
-		menu
-	}
-}
-
-fn format_title(text: &str) -> String {
-	let line = str::replace(text, "\n", "");
-	let end = line.chars().map(|c| c.len_utf8()).take(LINE_LENGTH).sum();
-	line[..end].to_string()
-}
-
-fn shift_fifo(string_to_add: &str, vector: &Vec<String>) -> Vec<String> {
-	let mut new_vect = vector.clone();
-	let len = vector.len();
-
-	let mut last_item = new_vect[0].clone();
-	new_vect[0] = string_to_add.to_string();
-
-	for i in 1..len {
-		if new_vect[i] == string_to_add {
-			new_vect[i] = last_item.clone();
-			break;
-		}
-		let temp = new_vect[i].clone();
-        new_vect[i] = last_item.clone();
-        last_item = temp.clone();
-    }
-
-	new_vect
-}
+use tray_icon::{Icon, TrayIconBuilder, menu::Menu};
+use image::DynamicImage;
+use image::GenericImageView;
 
 fn main() {
-	let mut clipboard = Clipboard::new().expect("Failed to initialize clipboard");
-	let mut last_item = String::new();
+    // Open the image file
+    if let Ok(img) = image::open("../ressources/icon.png") {
+        // Convert the image to RGBA
+        let rgba_image = img.to_rgba8();
 
-	let service = ksni::TrayService::new(MyTray {
-		items: vec!["".to_string(); BUFFER_LENGTH],
-	});
-	let handle = service.handle();
-	service.spawn();
+        // Do whatever you want with the RGBA image here
+        // For demonstration purposes, we'll just print its dimensions
+        let (width, height) = rgba_image.dimensions();
 
-	// Run forever
-	loop {
-		if let Ok(clipboard_text) = clipboard.get_text() {
-			if last_item != clipboard_text {
-				last_item = clipboard_text;
-				handle.update(|tray: &mut MyTray| {
-					tray.items = shift_fifo(&last_item, &tray.items);
-				});
-			}
-		}
+		let rgba_bytes: Vec<u8> = rgba_image.to_vec();
 
-		std::thread::sleep(std::time::Duration::from_millis(100));
-	}
-}
+		let maybe_icon = Icon::from_rgba(rgba_bytes, width, height);
 
-fn set_clipboard_text(txt: &str) {
-	let mut _clipboard = Clipboard::new().expect("Failed to initialize clipboard");
-	_clipboard.set_text(txt).unwrap();
+		if let Ok(icon) = maybe_icon {
+	 		let tray_menu = Menu::new();
+	       	let tray_icon = TrayIconBuilder::new()
+	       	    .with_menu(Box::new(tray_menu))
+	       	    .with_tooltip("system-tray - tray icon library!")
+	       	    .with_icon(icon)
+	       	    .build()
+	       	    .unwrap();
+	    } else {
+	    	println!("Failed to convert to icon");
+	    }
+ 		              
+    } else {
+        println!("Failed to open image file.");
+    }
 }
