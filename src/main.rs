8ee3b422cfe8a5a0e21e164f94ac98a4a6cc0415
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-08-07 17:34:06 +0200

    changed from listen to keys, to listen to signal

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use rdev::{grab, Event, EventType, Key, simulate};
-use tokio::sync::mpsc;
-use tokio::task;
-use tokio::process::Command;
-use tokio::io::AsyncWriteExt;
+use std::process::Command;
+use std::process::Stdio;
 use arboard::Clipboard;
 
-enum Hotkey {
-	ControlPressed,
-	ControlReleased,
-	ModPressed,
-	ModReleased,
-	KeyC,
-	KeyV,
-}
+use std::io::Error;
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 
 struct History {
 	entries: Vec<String>,
@@ -35,15 +29,16 @@ impl History {
 				return
 			}
 		}
-		
+		println!("new entry");
 		self.entries.insert(0, clip);
 	}
 	
-	async fn paste(&mut self) {
+	fn paste(&mut self) {
 		if self.entries.len() == 0 {
 			return
 		}
-		match self.select_entry().await {
+		println!("pasting");
+		match self.select_entry() {
 			Ok(entry) => {
 				let _ = self.clipboard.set_text(entry);
 			}
@@ -53,8 +48,7 @@ impl History {
 		}
 	}
 	
-	async fn select_entry(&self) -> Result<String, Box<dyn std::error::Error>> {
-		
+	fn select_entry(&self) -> Result<String, Error> {
 		
 		let mut formated_data : Vec<String> = vec![];
 		for (index, entry) in self.entries.iter().enumerate() {
@@ -62,25 +56,26 @@ impl History {
 		}
 
 		let input_data = formated_data.join("\n");
-		// Prepare the command
+		
 		let mut cmd = Command::new("sh")
-			.arg("-c")
-			.arg("marukuru -l 10")
-			.stdin(std::process::Stdio::piped())
-			.stdout(std::process::Stdio::piped())
-			.spawn()?;
+	        .arg("-c")
+	        .arg("marukuru -l 10")
+	        .stdin(Stdio::piped())
+	        .stdout(Stdio::piped())
+	        .spawn()?;
 		
-		let stdin = cmd.stdin.as_mut().ok_or("Failed to open stdin")?;
-		stdin.write_all(input_data.as_bytes()).await?;
-		
-		// Await the command to complete
-		let output = cmd.wait_with_output().await?;
-		
-		let result = String::from_utf8(output.stdout)?;
-		
-		let index_str = result.split('-').next().unwrap_or("0");
-		
-		let index = index_str.parse::<usize>().unwrap_or(0);
+		let stdin = cmd.stdin.as_mut().ok_or_else(|| Error::new(std::io::ErrorKind::Other, "Failed to open stdin"))?;
+	    stdin.write_all(input_data.as_bytes())?;
+	
+	    // Wait for the command to complete and get the output
+	    let output = cmd.wait_with_output()?;
+	
+	    // Convert the output to a string
+	    let result = String::from_utf8(output.stdout).unwrap_or("".to_string());
+	
+	    // Parse the result
+	    let index_str = result.split('-').next().unwrap_or("0");
+	    let index = index_str.parse::<usize>().unwrap_or(0);
 		
 		Ok(self.entries[index].clone())
 	}
@@ -88,81 +83,20 @@ impl History {
 
 
 
-#[tokio::main]
-async fn main() {
-	// Create a channel for communication between the grab callback and the main task
-	let (tx, mut rx) = mpsc::channel(100);
-
-	// Define the callback function
-	let callback = move |event: Event| -> Option<Event> {
-		let tx = tx.clone();
-		task::spawn(async move {
-			match event.event_type {
-				EventType::KeyPress(Key::KeyC) => {
-					tx.send(Hotkey::KeyC).await.unwrap();
-				}
-				EventType::KeyPress(Key::KeyV) => {
-					tx.send(Hotkey::KeyV).await.unwrap();
-				}
-				EventType::KeyPress(Key::MetaLeft) => {
-					tx.send(Hotkey::ModPressed).await.unwrap();
-				}
-				EventType::KeyRelease(Key::MetaLeft) => {
-					tx.send(Hotkey::ModReleased).await.unwrap();
-				}
-				EventType::KeyPress(Key::ControlLeft) => {
-					tx.send(Hotkey::ControlPressed).await.unwrap();
-				}
-				EventType::KeyRelease(Key::ControlLeft) => {
-					tx.send(Hotkey::ControlReleased).await.unwrap();
-				}
-				_ => {}
-			}
-		});
-		Some(event)
-	};
-	
-	let _ = simulate(&EventType::KeyRelease(Key::Return));
-
-	// Spawn a task to handle the event grabbing
-	tokio::spawn(async move {
-		loop {
-			if let Err(error) = grab(callback.clone()) {
-				println!("Error: {:?}", error);
-				return
-			}
-		}
-	});
-
-	let mut ctrl_pressed = false;
-	let mut mod_pressed = false;
+fn main() {
 	let mut history = History::new();
-	
-	// Main task to handle state updates
-	while let Some(hotkey) = rx.recv().await {
-		match hotkey {
-			Hotkey::ControlPressed => {
-				ctrl_pressed = true;
-			}
-			Hotkey::ControlReleased => {
-				ctrl_pressed = false;
-			}
-			Hotkey::ModPressed => {
-				mod_pressed = true;
-			}
-			Hotkey::ModReleased => {
-				mod_pressed = false;
-			}
-			Hotkey::KeyC => {
-				if ctrl_pressed {
-					history.update();
-				}
-			}
-			Hotkey::KeyV => {
-				if mod_pressed {
-					history.paste().await;
-				}
-			}
-		}
-	}
+
+	let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGUSR1, Arc::clone(&term)).unwrap();
+
+    loop {
+    	while !term.load(Ordering::Relaxed) {
+    		std::thread::sleep(std::time::Duration::from_secs(2));
+    		history.update();
+    	}
+
+    	history.paste();
+
+    	term.store(false, Ordering::Relaxed);
+    }
 }
